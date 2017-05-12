47467161bf9f0f714e0bae80a2bd48becb2de5ce
drm2 <davidrmyersii@gmail.com>
2017-05-12 17:52:34 -0400

    Add initial solution for rotational-cipher

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -1,4 +1,6 @@
 defmodule RotationalCipher do
+  @lower ?a..?z
+  @upper ?A..?Z
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,6 +10,15 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    to_charlist(text)
+    |> Enum.map(fn(char) -> cycle(char, shift) end)
+    |> to_string
+  end
+
+  defp cycle(char, shift) when char in @lower, do: cycle(char, shift, @lower)
+  defp cycle(char, shift) when char in @upper, do: cycle(char, shift, @upper)
+  defp cycle(char, _), do: char
+  defp cycle(char, shift, chars) do
+    rem(char - Enum.at(chars, 0) + shift, Enum.count(chars)) + Enum.at(chars, 0)
   end
 end
-
